93c65da4891bdc80f2b930a48cbf2c5b9851baea
Andrei Gaspar <andrei@ildiesign.com>
2018-10-12 19:57:51 +0300

    - Object type modals and listeners

--- src/object-types/ObjectTypes.ts
+++ src/object-types/ObjectTypes.ts
@@ -18,11 +18,29 @@ export class ObjectTypes extends ViewComponent {
     private objectTypesContainer: HTMLElement;
     private addBtn: HTMLButtonElement;
     private modalBackground: HTMLElement;
+
+    private modalOT: HTMLElement;
     private modalOTNameInput: HTMLInputElement;
     private modalOTPropertiesContainer: HTMLElement;
-    private modalOTAddPropertiesBtn: HTMLButtonElement;
+    private modalOTEditPropertiesBtn: HTMLButtonElement;
     private modalOTCancelBtn: HTMLButtonElement;
     private modalOTOKBtn: HTMLButtonElement;
+
+
+    private modalEditPD: HTMLElement;
+    private modalEditPDPropertyContainer: HTMLElement;
+    private modalEditPDNewPropBtn: HTMLButtonElement;
+    private modalEditPDCancelBtn: HTMLButtonElement;
+    private modalEditPDOKBtn: HTMLButtonElement;
+
+
+    private modalNewPD: HTMLElement;
+    private modalNewPDNameInput: HTMLInputElement;
+    private modalNewPDDataTypeSelect: HTMLSelectElement;
+    private modalNewPDObjectTypeSelect: HTMLSelectElement;
+    private modalNewPDCancelBtn: HTMLButtonElement;
+    private modalNewPDOKBtn: HTMLButtonElement;
+
 
 
 
@@ -44,18 +62,37 @@ export class ObjectTypes extends ViewComponent {
 
         this.container.appendChild( this.modalBackground );
 
-
+        this.modalOT                    = document.getElementById( "object-types-modal-container" );
         this.modalOTNameInput           = document.getElementById( "ot-modal-input-name" ) as HTMLInputElement;
         this.modalOTPropertiesContainer = document.getElementById( "ot-modal-properties-container" );
-        this.modalOTAddPropertiesBtn    = document.getElementById( "ot-modal-add-property-btn" ) as HTMLButtonElement;
-        this.modalOTCancelBtn           = document.getElementById( "ot-modal-ok-btn" ) as HTMLButtonElement;
-        this.modalOTOKBtn               = document.getElementById( "ot-modal-cancel-btn" ) as HTMLButtonElement;
+        this.modalOTEditPropertiesBtn   = document.getElementById( "ot-modal-edit-property-btn" ) as HTMLButtonElement;
+        this.modalOTCancelBtn           = document.getElementById( "ot-modal-cancel-btn" ) as HTMLButtonElement;
+        this.modalOTOKBtn               = document.getElementById( "ot-modal-ok-btn" ) as HTMLButtonElement;
 
 
-        this.modalBackgroundListener    = this.modalBackgroundListener.bind( this );
-        this.addBtnListener             = this.addBtnListener.bind( this );
-        this.cancelBtnListener          = this.cancelBtnListener.bind( this );
+        this.modalEditPD                     = document.getElementById( "ot-add-properties-modal-container" );
+        this.modalEditPDPropertyContainer    = document.getElementById( "ot-add-properties-modal-property-container" );
+        this.modalEditPDNewPropBtn           = document.getElementById( "ot-add-properties-modal-new-property-btn" ) as HTMLButtonElement;
+        this.modalEditPDCancelBtn            = document.getElementById( "ot-add-properties-cancel-btn" ) as HTMLButtonElement;
+        this.modalEditPDOKBtn                = document.getElementById( "ot-add-properties-ok-btn" ) as HTMLButtonElement;
 
+
+        this.modalNewPD                     = document.getElementById( "property-definitions-modal-container" );
+        this.modalNewPDNameInput            = document.getElementById( "pd-modal-input-name" ) as HTMLInputElement;
+        this.modalNewPDDataTypeSelect       = document.getElementById( "pd-modal-select-data-type" ) as HTMLSelectElement;
+        this.modalNewPDObjectTypeSelect     = document.getElementById( "pd-modal-select-object-type" ) as HTMLSelectElement;
+        this.modalNewPDCancelBtn            = document.getElementById( "pd-modal-add-cancel" ) as HTMLButtonElement;
+        this.modalNewPDOKBtn                = document.getElementById( "pd-modal-add-ok" ) as HTMLButtonElement;
+
+
+
+        this.modalBackgroundListener            = this.modalBackgroundListener.bind( this );
+        this.modalOTaddBtnListener              = this.modalOTaddBtnListener.bind( this );
+        this.modalOTCancelBtnListener           = this.modalOTCancelBtnListener.bind( this );
+        this.modalOTEditPropertiesListener      = this.modalOTEditPropertiesListener.bind( this );
+        this.modalEditPDCancelBtnListener       = this.modalEditPDCancelBtnListener.bind( this );
+        this.modalEditPDNewPropBtnListener      = this.modalEditPDNewPropBtnListener.bind( this );
+        this.modalNewPDCancelBtnListener        = this.modalNewPDCancelBtnListener.bind( this );
 
         this.enterScene();
     }
@@ -64,9 +101,13 @@ export class ObjectTypes extends ViewComponent {
 
     private registerEventListeners(): void {
 
-        this.addBtn.addEventListener( "click", this.addBtnListener );
+        this.addBtn.addEventListener( "click", this.modalOTaddBtnListener );
         this.modalBackground.addEventListener( "click", this.modalBackgroundListener );
-        this.modalOTCancelBtn.addEventListener( "click", this.cancelBtnListener );
+        this.modalOTCancelBtn.addEventListener( "click", this.modalOTCancelBtnListener );
+        this.modalOTEditPropertiesBtn.addEventListener( "click", this.modalOTEditPropertiesListener );
+        this.modalEditPDCancelBtn.addEventListener( "click", this.modalEditPDCancelBtnListener );
+        this.modalEditPDNewPropBtn.addEventListener( "click", this.modalEditPDNewPropBtnListener );
+        this.modalNewPDCancelBtn.addEventListener( "click", this.modalNewPDCancelBtnListener );
 
 
     }
@@ -75,34 +116,67 @@ export class ObjectTypes extends ViewComponent {
 
     private unregisterEventListeners(): void {
 
-        this.addBtn.removeEventListener( "click", this.addBtnListener );
+        this.addBtn.removeEventListener( "click", this.modalOTaddBtnListener );
         this.modalBackground.removeEventListener( "click", this.modalBackgroundListener );
-        this.modalOTCancelBtn.removeEventListener( "click", this.cancelBtnListener );
+        this.modalOTCancelBtn.removeEventListener( "click", this.modalOTCancelBtnListener );
+        this.modalOTEditPropertiesBtn.removeEventListener( "click", this.modalOTEditPropertiesListener );
+        this.modalEditPDCancelBtn.removeEventListener( "click", this.modalEditPDCancelBtnListener );
+        this.modalEditPDNewPropBtn.removeEventListener( "click", this.modalEditPDNewPropBtnListener );
+        this.modalNewPDCancelBtn.removeEventListener( "click", this.modalNewPDCancelBtnListener );
 
     }
 
 
 
-    private addBtnListener(e: any): void {
+    private modalOTaddBtnListener(e: any): void {
         this.modalBackground.style.display = "block";
     }
 
 
 
     private modalBackgroundListener(e: any): void {
-        if ( e.target.id === this.modalBackground.id ) this.hideNewPropDefModal();
+        if ( e.target.id === this.modalBackground.id ) this.hideModals();
     }
 
 
-    private cancelBtnListener(e: any): void {
-        this.hideNewPropDefModal();
+    private modalOTCancelBtnListener(e: any): void {
+
+        console.info( "cancel btn clicked" );
+        this.hideModals();
     }
 
 
-    private hideNewPropDefModal(): void {
-        this.modalBackground.style.display = "none";
+
+    private modalOTEditPropertiesListener(e: any): void {
+        this.modalOT.style.display = "none";
+        this.modalEditPD.style.display = "block";
     }
 
+
+    private modalEditPDCancelBtnListener(e: any): void {
+        this.modalEditPD.style.display = "none";
+        this.modalOT.style.display = "block";
+    }
+
+
+    private modalEditPDNewPropBtnListener(e: any): void {
+        this.modalEditPD.style.display = "none";
+        this.modalNewPD.style.display = "block";
+    }
+
+
+    private modalNewPDCancelBtnListener(e: any): void {
+        this.modalNewPD.style.display = "none";
+        this.modalEditPD.style.display = "block";
+    }
+
+
+    private hideModals(): void {
+        this.modalBackground.style.display  = "none";
+        this.modalEditPD.style.display      = "none";
+        this.modalNewPD.style.display       = "none";
+        this.modalOT.style.display          = "block";
+    }
 
 
     public enterScene(): void {
@@ -152,6 +226,31 @@ export class ObjectTypes extends ViewComponent {
                     this.objectTypesContainer.appendChild( otItem );
 
                 }
+
+            },
+            (message: string) => {
+                console.warn( message );
+            }
+        )
+
+
+    }
+
+
+
+    private populateEditPropertiesModal(): void {
+
+        this.connection.getPropertyDefinitions(
+            (response: any) => {
+                const { properties } = response;
+
+                for ( let i = 0; i < properties.length; i++ ) {
+
+                    // TODO
+
+
+                }
+
 
             },
             (message: string) => {
